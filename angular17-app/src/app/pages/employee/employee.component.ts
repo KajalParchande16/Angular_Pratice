e2b2aa25b19afbfdd61817cec3ae9780316051ef
KajalParchande16 <parchandekajal16@gmail.com>
2026-01-08 14:50:07 +0000

    common modal & dynaymic form control added

--- angular17-app/src/app/pages/employee/employee.component.ts
+++ angular17-app/src/app/pages/employee/employee.component.ts
@@ -1,61 +1,137 @@
 import { Component, inject } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { UtilityService } from '../../core/utility.service';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { TempErrorComponent } from '../../shared/temp-error/temp-error.component';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, TempErrorComponent, NgSelectModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
 export class EmployeeComponent {
-  modalService=inject(NgbModal);
-modal:any;
-dynamicModal=inject(UtilityService);
+  modalService = inject(NgbModal);
+  modal: any;
+  dynamicModal = inject(UtilityService);
 
-  openModal(c:any)
-  {
-this.modal=this.modalService.open(c,{
-  size:'lg',
-  backdrop:'static',
-  keyboard:false,
-  windowClass:'modal-sm'
-
-})
+  empObj: Employee = new Employee();
+  showError: boolean = false;
+  selectedProject: any;
+  projectList: any[] = [
+    {
+      id: 1,
+      name: 'CRM'
+    },
+    {
+      id: 2,
+      name: 'ERP'
+    },
+    {
+      id: 3,
+      name: 'Deal-Distributor'
+    },
+    {
+      id: 4,
+      name: 'Website'
+    }
+  ]
+  onProjectChange(id: any) {
+    this.empObj.tasks = [];
+    if (!!id) {
+      this.addTaskRow();
+    }
+    else {
+    }
+    // console.log(id);
+    // console.log(this.empObj)
   }
 
-  openModal1(comp:any)
-  {
-    this.dynamicModal.open(comp,'lg','right')
+  addTaskRow() {
+    this.empObj.tasks.push(new Tasks());
   }
 
-  openLargeModal(c:any)
-  {
-    this.dynamicModal.open(c,'lg','left')
+  removeTaskRow(index: number) {
+    this.empObj.tasks.splice(index, 1);
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      this.showError = true;
+      return;
+    }
+    console.log(this.empObj); // form data is ready
+  }
+
+  openModal(c: any) {
+    this.modal = this.modalService.open(c, {
+      size: 'lg',
+      backdrop: 'static',
+      keyboard: false,
+      windowClass: 'modal-sm'
+
+    })
+  }
+
+  openModal1(comp: any) {
+    this.dynamicModal.open(comp, 'lg', 'right')
+  }
+
+  openLargeModal(c: any) {
+    this.dynamicModal.open(c, 'lg', 'left')
 
   }
 
-   openLargeBottomModal(c:any)
-  {
-    this.dynamicModal.open(c,'lg','bottom')
+  openLargeBottomModal(c: any) {
+    this.dynamicModal.open(c, 'lg', 'bottom')
 
   }
-   openLargeCenterModal(c:any)
-  {
-    this.dynamicModal.open(c,'lg')
+  openLargeCenterModal(c: any) {
+    this.dynamicModal.open(c, 'lg')
 
   }
-   openLargeTopModal(c:any)
-  {
-    this.dynamicModal.open(c,'lg','top')
+  openLargeTopModal(c: any) {
+    this.dynamicModal.open(c, 'lg', 'top')
 
   }
-  openLargeFullScreenModal(c:any)
-  {
-    this.dynamicModal.open(c,'lg','center',{
-      fullscreen:true
+  openLargeFullScreenModal(c: any) {
+    this.dynamicModal.open(c, 'lg', 'center', {
+      fullscreen: true
     })
 
   }
+}
+
+
+export class Employee {
+  id = 0;
+  name = "";
+  email = "";
+  mobileNo = "";
+  state = "";
+  city = "";
+  address = "";
+  projectId = null;
+  tasks: Tasks[] = [];
+
+  constructor(initial?: Partial<Employee>) {
+    Object.assign(this, initial)
+  }
+
+}
+
+export interface Project {
+  id: number,
+  name: string,
+  task?: Tasks
+}
+
+export class Tasks {
+  id: number | null = null;
+  taskName = "";
+  duration: number | null = null;
+
 }
