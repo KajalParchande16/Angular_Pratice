a6d906db96e2e9b44451dc402b9924cb03c9d765
kajalblog <kajalparchandee.com>
2026-03-21 11:37:35 +0530

    bucket UI Implemented

--- angular17-app/src/app/pages/bucket/bucket.component.ts
+++ angular17-app/src/app/pages/bucket/bucket.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Bucket } from '../../shared/model/bucket';
+import { select, Store } from '@ngrx/store';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-bucket',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './bucket.component.html',
   styleUrl: './bucket.component.css'
 })
 export class BucketComponent {
+  bucketList$:Observable<Bucket[]>=new Observable();
+  constructor(private store:Store<{bucket:Bucket[]}>)
+  {
+    this.bucketList$=store.pipe(select('bucket'));
+  }
 
 }
