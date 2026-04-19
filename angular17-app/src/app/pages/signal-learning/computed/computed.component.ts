931d33769f2b6bda19a64ef9a7f60e500fce970b
kajalblog <kajalparchandee.com>
2026-04-19 23:50:55 +0530

    computed signal

---
+++ angular17-app/src/app/pages/signal-learning/computed/computed.component.ts
@@ -0,0 +1,33 @@
+import { Component, computed, Signal, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-computed',
+  standalone: true,
+  imports: [],
+  templateUrl: './computed.component.html',
+  styleUrl: './computed.component.css'
+})
+export class ComputedComponent {
+  x=20;
+  y=10;
+  z=this.x+this.y;
+  p=signal(50);
+  q=signal(20);
+  // r=this.p()+this.q();
+  count:Signal<any>=computed(()=>{
+    return this.p()+this.q();
+  })
+
+  // in normal value update value not calculate so computed signal used
+  showFinalValue()
+  {
+    console.log(this.z);
+    this.x=100;
+    console.log(this.z);
+    console.log(this.count());
+    this.p.set(200);
+
+    console.log(this.count());
+
+  }
+}
